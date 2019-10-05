cf30e6917fbba5a34f588f2fbba190a2d83991fe
heyang <heyanggo@gmail.com>
2019-10-05 17:02:42 +0800

    使用parcel打包工具，项目框架搭建

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # tione
 一个基于vue的UI组件库
+
+## 安装
+使用本框架前，请在css中开启 border-box
+
+```
+#{box-sizing: border-box;}
+```
---
+++ src/button.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button class="g-button">按钮</button>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "button",
+    }
+</script>
+
+<style lang="scss" scoped>
+    .g-button{
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+    }
+    .g-button:hover{
+        border-color: var(--border-color-hover);
+    }
+    .g-button:active{
+        background-color: var(--button-active-bg);
+    }
+    .g-button:focus{
+        outline: none;
+    }
+</style>
