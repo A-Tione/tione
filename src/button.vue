8c2367bdfd84a633aca0035fc74a8a01e7614616
heyang <heyanggo@gmail.com>
2019-10-06 11:39:25 +0800

    添加loading 加载中状态

--- src/button.vue
+++ src/button.vue
@@ -1,17 +1,18 @@
 <template>
-        <button class="t-button" :class="{[`icon-${iconPosition}`]: true}">
-            <t-icon class="icon" v-if="icon" :name="icon"></t-icon>
-            <div class="content">
-                <slot></slot>
-            </div>
-        </button>
+    <button class="t-button" :class="{[`icon-${iconPosition}`]: true}">
+        <t-icon class="icon" v-if="icon" :name="icon"></t-icon>
+        <t-icon class="loading"  name="loading"></t-icon>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
     export default {
-        props:{
-            icon:{},
-            iconPosition:{
+        props: {
+            icon: {},
+            iconPosition: {
                 type: String,
                 default: 'left',
                 validator: function (value) {//属性检查器
@@ -24,7 +25,7 @@
 </script>
 
 <style lang="scss" scoped>
-    .t-button{
+    .t-button {
         display: inline-flex;
         justify-content: center;
         align-items: center;
@@ -35,34 +36,45 @@
         border: 1px solid var(--border-color);
         background: var(--button-bg);
         vertical-align: top;
-        &:hover{
+        &:hover {
             border-color: var(--border-color-hover);
         }
-        &:active{
+        &:active {
             background-color: var(--button-active-bg);
         }
-        &:focus{
+        &:focus {
             outline: none;
         }
-        > .icon{
+        > .icon {
             order: 1;
             margin-right: .1em;
             margin-left: 0;
         }
-        > .content{
+        > .content {
             order: 2;
             line-height: 100%;
         }
-        &.icon-right{
-            > .content{
+        &.icon-right {
+            > .content {
                 order: 1;
                 line-height: 100%;
             }
-            > .icon{
+            > .icon {
                 order: 2;
                 margin-left: .1em;
                 margin-right: 0;
             }
         }
+    }
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg)
+        }
+    }
+    .loading {
+        animation: spin 1.5s infinite linear;
     }
 </style>
