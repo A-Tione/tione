53f4d2578d2453413ac9e91043c73efb57d5c81b
heyang <heyanggo@gmail.com>
2019-10-09 22:07:47 +0800

    使用chai添加四个测试用例

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+
 Vue.component('t-button',Button)
 Vue.component('t-icon',Icon)
 Vue.component('t-button-group',ButtonGroup)
@@ -11,6 +12,92 @@ new Vue({
     data: {
         loading1: false,
         loading2: false,
-        loading3: false
+        loading3: true
     }
 });
+
+//单元测试  chai库
+import  chai from 'chai'
+const expect = chai.expect
+
+// icon
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    //断言href等于#i-settings,一个输入等于一个输出，没有报错则测试通过
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.equal('#i-settings')
+    vm.$el.remove()
+    vm.$destroy()
+}
+// loading
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading: true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.equal('#i-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+// icon的前后顺序
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.equal('1')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.equal('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
+// button 点击事件
+{
+    //mock
+    const Constructor = Vue.extend(Button)
+    const tButton = new Constructor({
+        propsData: {
+            icon: 'settings',
+        }
+    })
+    tButton.$mount()
+
+    // 希望这个函数被执行
+    let button = tButton.$el
+    button.click()
+}
