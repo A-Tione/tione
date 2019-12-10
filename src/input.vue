3d2ecdfb4117ffd258a81e672eae223755e30e04
heyang <heyanaggo@gmail.com>
2019-12-10 12:01:10 +0800

    input.vue组件增加样式

--- src/input.vue
+++ src/input.vue
@@ -1,22 +1,90 @@
 <template>
-    <div class="wrapper">
-        <input type="text">
+    <div class="wrapper" :class="{error}">
+        <input type="text"
+               :value="value"
+               :disabled="disabled"
+               :readonly="readonly">
+        <template>
+            <icon name="settings"></icon>
+            <span>{{error}}</span>
+        </template>
     </div>
 </template>
 
 <script>
+    import Icon from './icon'
+
     export default {
-        name: "tInput"
+        components:{
+            Icon
+        },
+        name: "tInput",
+        props: {
+            value: {
+                type: String,
+                default() {
+                    return '';
+                }
+            },
+            disabled: {
+                type: Boolean,
+                default() {
+                    return false;
+                }
+            },
+            readonly: {
+                type: Boolean,
+                default() {
+                    return false;
+                }
+            },
+            error: {
+                type: String
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
     $height: 32px;
     $border-color: #999;
-    .wrapper{
-        >input{
+    $border-color-hover: rgba(0,0,0,0.6);
+    $border-radius: 4px;
+    $font-size: 12px;
+    $box-shadow-color: rgba(0,0,0,0.5);
+    $red: #cc0000;
+    .wrapper {
+        font-size: $font-size;
+        display: inline-block;
+        > input {
             height: $height;
-            border-color: $border-color;
+            border: 1px solid $border-color;
+            border-radius: $border-radius;
+            padding: 0 8px;
+            &:hover {
+                border-color: $border-color-hover;
+            }
+            &:focus {
+                box-shadow: inset 0 1px 3px $box-shadow-color;
+                outline: none;
+            }
+            &[disabled] {
+                border-color: #909090;
+                color: #909090;
+                background: #eeeeee;
+                cursor: not-allowed;
+            }
+            &[readonly] {
+                cursor: not-allowed;
+            }
+        }
+        &.error{
+            >input{
+                border-color: $red;
+                &:focus {
+                    box-shadow: 0 0 2px 0 $red;
+                }
+            }
         }
     }
 </style>
