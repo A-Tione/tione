dde1580476732b764ee2dcfcd73bfbbfe3513c74
a-tione <heyanggo@gmail.com>
2019-12-16 22:13:02 +0800

    布局组件div判断左右布局

--- src/layout.vue
+++ src/layout.vue
@@ -32,7 +32,6 @@
         flex-grow: 1;
         display: flex;
         flex-direction: column;
-        border: 1px solid black ;
         &.hasSider {
             flex-direction: row;
         }
