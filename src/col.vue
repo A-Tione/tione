29112219ae15c66c79b86d419496b1e59198128e
a-tione <heyanggo@gmail.com>
2019-12-23 21:26:49 +0800

    修复col组件代码，并测试成功

--- src/col.vue
+++ src/col.vue
@@ -9,7 +9,7 @@
         let keys = Object.keys(value)
         let valid = true
         keys.forEach(key => {
-            if (!['span', 'offset', 'ipad', 'narrow', 'pc', 'wide'].indexOf(key) >= 0) {
+            if (!(['span', 'offset', 'ipad', 'narrow', 'pc', 'wide'].indexOf(key) >= 0)) {
                 valid = false
             }
         })
