e8e3b8feec8b3f4f8fb9e04bec45be3e47a47f9f
a-tione <heyanggo@gmail.com>
2019-12-29 22:03:09 +0800

    解决当容器有overflow:hidden 的BUG

--- src/popover.vue
+++ src/popover.vue
@@ -32,8 +32,8 @@
                     setTimeout(() => {
                     document.body.appendChild(this.$refs.contentWrapper)
                         let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-                        this.$refs.contentWrapper.style.left = left + 'px'
-                        this.$refs.contentWrapper.style.top = top + 'px'
+                        this.$refs.contentWrapper.style.left = left+ window.scrollX + 'px'
+                        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
                         let x = () => {
                             this.visible = false
                             document.removeEventListener('click', x)
