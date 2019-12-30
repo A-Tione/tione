2085e19c65d555c00662db2c905bc2f6437b5b79
a-tione <heyanggo@gmail.com>
2019-12-30 21:59:59 +0800

    popover 四个方向的位置样式

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
     <div ref="popover" class="popover" @click="onClick">
-        <div v-if="visible" ref="contentWrapper" class="content-wrapper">
+        <div v-if="visible" ref="contentWrapper" class="content-wrapper" :class="{[`position-${position}`]: true}">
             <slot name="content"></slot>
         </div>
         <!-- span标签增加display: inline-block; 解决包裹元素高度一致的问题 -->
@@ -15,7 +15,15 @@
         name: "tPopover",
         components: {},
 
-        props: {},
+        props: {
+            position: {
+                type: String,
+                default: 'top',
+                validaor(value) {
+                    return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+                }
+            }
+        },
 
         data() {
             return {
@@ -29,9 +37,21 @@
         methods: {
             positionContent() {
                 document.body.appendChild(this.$refs.contentWrapper)
-                let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-                this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-                this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+                const {contentWrapper,triggerWrapper} = this.$refs
+                let {width, height, top, left} = triggerWrapper.getBoundingClientRect()
+                if (this.position === 'top') {
+                    contentWrapper.style.left = left + window.scrollX + 'px'
+                    this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+                } else if (this.position === 'bottom') {
+                    contentWrapper.style.left = left + window.scrollX + 'px'
+                    contentWrapper.style.top = top + height + window.scrollY + 'px'
+                } else if (this.position === 'left') {
+                    contentWrapper.style.left = left + window.scrollX + 'px'
+                    contentWrapper.style.top = top + window.scrollY + 'px'
+                } else if (this.position === 'right') {
+                    contentWrapper.style.left = left+ width + window.scrollX + 'px'
+                    contentWrapper.style.top = top + window.scrollY + 'px'
+                }
             },
             onClickDocument(e) { // 如果点击在popover 则让popover自己去处理，document不管
                 if (this.$refs.contentWrapper && this.$refs.contentWrapper.contains(e.target)) {return}
@@ -72,12 +92,9 @@
     .content-wrapper {
         position: absolute;
         padding: .5em 1em;
-        margin-top: -10px;
         background: white;
-        transform: translateY(-100%);
         border: 1px solid $border-color;
         border-radius: $border-radius;
-        /*box-shadow: 0 0 1 px rgba(0, 0, 0, 0.5);*/
         filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5)); // 伪类增加阴影，兼容性差IE、Opera Mini不支持
         max-width: 20em;
         word-wrap: break-word;
@@ -85,12 +102,43 @@
             content: '';
             display: block;
             border: 10px solid transparent;
-            border-top-color: white;
             width: 0;
             height: 0;
             position: absolute;
-            left: 10px;
-            top: 100%;
+        }
+        &.position-top {
+            margin-top: -10px;
+            transform: translateY(-100%);
+            &::before {
+                left: 10px;
+                top: 100%;
+                border-top-color: white;
+            }
+        }
+        &.position-bottom {
+            margin-top: 10px;
+            &::before {
+                left: 10px;
+                bottom: 100%;
+                border-bottom-color: white;
+            }
+        }
+        &.position-left {
+            margin-left: -10px;
+            transform: translateX(-100%);
+            &::before {
+                left: 100%;
+                top: 7px;
+                border-left-color: white;
+            }
+        }
+        &.position-right {
+            margin-left: 10px;
+            &::before {
+                right: 100%;
+                top: 7px;
+                border-right-color: white;
+            }
         }
     }
 </style>
