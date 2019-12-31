db7851ea2dd31abfe7c24f492b4c78a0fad89f2e
a-tione <heyanggo@gmail.com>
2019-12-31 16:57:30 +0800

    popover 测试用例trigger有问题，待完善

--- test/popover.test.js
+++ test/popover.test.js
@@ -13,7 +13,28 @@ describe('popover', () => {
         expect(Popover).to.exist
     })
 
-    it('可以设置 position.', (done) => {
+    it('可以设置position的top', (done) => {
+        Vue.component('t-button', Button)
+        Vue.component('t-popover', Popover)
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        div.innerHTML = `
+        <t-popover position="top" ref="xxx">
+            <template slot="content">
+                <div>popover内容</div>
+            </template>
+                <t-button>按钮</t-button>
+        </t-popover>`
+        const vm = new Vue({
+            el: div
+        })
+        vm.$el.querySelector('button').click()
+        vm.$nextTick(() => {
+            expect(vm.$refs.xxx.$refs.contentWrapper.classList.contains('position-top')).to.be.true;
+            done()
+        })
+    })
+    it('可以设置position的bottom', (done) => {
         Vue.component('t-button', Button)
         Vue.component('t-popover', Popover)
         const div = document.createElement('div')
@@ -30,9 +51,74 @@ describe('popover', () => {
         })
         vm.$el.querySelector('button').click()
         vm.$nextTick(() => {
-            let {contentWrapper} = vm.$refs.xxx.$refs.contentWrapper
-            expect(contentWrapper.classList.contains('position-bottom')).to.be.true;
+            expect(vm.$refs.xxx.$refs.contentWrapper.classList.contains('position-bottom')).to.be.true;
             done()
+        })
+    })
+    it('可以设置position的left', (done) => {
+        Vue.component('t-button', Button)
+        Vue.component('t-popover', Popover)
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        div.innerHTML = `
+        <t-popover position="left" ref="xxx">
+            <template slot="content">
+                <div>popover内容</div>
+            </template>
+                <t-button>按钮</t-button>
+        </t-popover>`
+        const vm = new Vue({
+            el: div
+        })
+        vm.$el.querySelector('button').click()
+        vm.$nextTick(() => {
+            expect(vm.$refs.xxx.$refs.contentWrapper.classList.contains('position-left')).to.be.true;
+            done()
+        })
+    })
+    it('可以设置position的right', (done) => {
+        Vue.component('t-button', Button)
+        Vue.component('t-popover', Popover)
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        div.innerHTML = `
+        <t-popover position="right" ref="xxx">
+            <template slot="content">
+                <div>popover内容</div>
+            </template>
+                <t-button>按钮</t-button>
+        </t-popover>`
+        const vm = new Vue({
+            el: div
+        })
+        vm.$el.querySelector('button').click()
+        vm.$nextTick(() => {
+            expect(vm.$refs.xxx.$refs.contentWrapper.classList.contains('position-right')).to.be.true;
+            done()
+        })
+    })
+    xit('可以设置 trigger', (done) => {
+        Vue.component('t-button', Button)
+        Vue.component('t-popover', Popover)
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        div.innerHTML = `
+        <t-popover tirgger="hover" ref="xxx">
+            <template slot="content">
+                <div>popover内容</div>
+            </template>
+                <t-button>按钮</t-button>
+        </t-popover>`
+        const vm = new Vue({
+            el: div
+        })
+        vm.$nextTick(() => {
+            let event = new Event('mouseenter')
+            vm.$el.dispatchEvent(event)// 触发hover事件
+            vm.$nextTick(() => {
+                expect(vm.$refs.xxx.$refs.contentWrapper).to.exist
+                done()
+            })
         })
     })
 })
