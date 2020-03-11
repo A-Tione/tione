214d2331828fe9dcf65eb0f44e830c40355edb2b
a-tione <heyanggo@gmail.com>
2020-03-11 11:10:26 +0800

    添加layout组件示例

---
+++ docs/.vuepress/components/layout-demo-3.vue
@@ -0,0 +1,68 @@
+<template>
+    <div style="paddint-top: 16px;">
+        <h2>简单用法</h2>
+        <p>
+            <strong>预览</strong>
+        </p>
+        <t-layout style="color: white; overflow:hidden; margin-bottom:50px;">
+            <t-sider style=" background:#efefef; width:200px; color: black;">
+                sider
+            </t-sider>
+            <t-layout>
+                <t-header style="height: 50px; background:orange;">
+                    header
+                </t-header>
+                <t-content style="height: 100px; background:orangered;">
+                    content
+                </t-content>
+                <t-footer style="height: 50px; background:orange;">
+                    footer
+                </t-footer>
+            </t-layout>
+        </t-layout>
+
+        <p>
+            <strong>代码</strong>
+        </p>
+        <pre><code>{{content}}</code></pre>
+    </div>
+</template>
+<script>
+    import TContent from '../../../src/layout/content'
+    import TFooter from '../../../src/layout/footer'
+    import THeader from '../../../src/layout/header'
+    import TLayout from '../../../src/layout/layout'
+    import TSider from '../../../src/layout/sider'
+
+    export default {
+        components: {
+            TContent,
+            TFooter,
+            THeader,
+            TLayout,
+            TSider
+        },
+        data() {
+            return {
+                content: `
+        <t-layout style="color: white; overflow:hidden; margin-bottom:50px;">
+                <t-sider style=" background:#efefef; width:200px; color: black;">
+                    sider
+                </t-sider>
+                <t-layout>
+                    <t-header style="height: 50px; background:orange;">
+                        header
+                    </t-header>
+                    <t-content style="height: 100px; background:orangered;">
+                        content
+                    </t-content>
+                    <t-footer style="height: 50px; background:orange;">
+                        footer
+                    </t-footer>
+                </t-layout>
+    </t-layout>
+      `.replace(/^ {8}/gm, '').trim()
+            }
+        }
+    }
+</script>
