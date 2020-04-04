ec810e37d0f3a9a763877b9b7f0bae77a33baf73
a-tione <heyanggo@gmail.com>
2020-04-04 22:18:10 +0800

    vue.config.js 代码格式化

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 const path = require('path')
 module.exports = {
-  css: {
-    loaderOptions: {
-      sass: {
-        includePaths: [path.join(__dirname, 'styles')]
-      },
+    css: {
+        loaderOptions: {
+            sass: {
+                includePaths: [path.join(__dirname, 'styles')]
+            }
+        }
     }
-  },
 }
