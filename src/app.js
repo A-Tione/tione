146f5ee9fa262862f54b6f75914133ccbc7b916f
a-tione <heyanggo@gmail.com>
2020-04-12 11:41:34 +0800

    cascader 递归组件，无需知道几次for循环

--- src/app.js
+++ src/app.js
@@ -20,40 +20,83 @@ import tTabsPane from './tabs/tabs-pane'
 import tPopover from './popover/popover'
 import tCollapse from './collapse/collapse'
 import tCollapseItem from './collapse/collapse-item'
-
+import tCascader from './cascader/cascader'
 Vue.use(tPlugin)
+
 Vue.component('t-button', tButton)
-Vue.component('t-icon', tIcon)
 Vue.component('t-button-group', tButtonGroup)
-Vue.component('t-input', tInput)
-Vue.component('t-row', tRow)
+Vue.component('t-cascader', tCascader)
 Vue.component('t-col', tCol)
-Vue.component('t-layout', tLayout)
-Vue.component('t-sider', tSider)
-Vue.component('t-header', tHeader)
-Vue.component('t-content', tContent)
-Vue.component('t-footer', tFooter)
-Vue.component('t-toast', tToast)
-Vue.component('t-tabs', tTabs)
-Vue.component('t-tabs-head', tTabsHead)
-Vue.component('t-tabs-body', tTabsBody)
-Vue.component('t-tabs-item', tTabsItem)
-Vue.component('t-tabs-pane', tTabsPane)
-Vue.component('t-popover', tPopover)
 Vue.component('t-collapse', tCollapse)
 Vue.component('t-collapse-item', tCollapseItem)
+Vue.component('t-content', tContent)
+Vue.component('t-footer', tFooter)
+Vue.component('t-header', tHeader)
+Vue.component('t-icon', tIcon)
+Vue.component('t-input', tInput)
+Vue.component('t-layout', tLayout)
+Vue.component('t-popover', tPopover)
+Vue.component('t-row', tRow)
+Vue.component('t-sider', tSider)
+Vue.component('t-tabs', tTabs)
+Vue.component('t-tabs-body', tTabsBody)
+Vue.component('t-tabs-head', tTabsHead)
+Vue.component('t-tabs-item', tTabsItem)
+Vue.component('t-tabs-pane', tTabsPane)
+Vue.component('t-toast', tToast)
 
 new Vue({
     el: '#app',
     data: {
-        selectedArray: ['1','2'],
+        source: [
+            {
+                name: '浙江',
+                children: [
+                    {
+                        name: '杭州',
+                        children: [
+                            {name: 'shang'},
+                            {name: 'xia'},
+                            {name: 'jianggan'}
+                        ]
+                    },
+                    {
+                        name: '嘉兴',
+                        children: [
+                            {name: 'shang'},
+                            {name: 'xia'},
+                            {name: 'jianggan'}
+                        ]
+                    }
+                ]
+            },
+            {
+                name: '湖南',
+                children: [
+                    {
+                        name: '长沙',
+                        children: [
+                            {name: 'shang'},
+                            {name: 'xia'},
+                            {name: 'jianggan'}
+                        ]
+                    },
+                    {
+                        name: '湘潭',
+                        children: [
+                            {name: 'shang'},
+                            {name: 'xia'},
+                            {name: 'jianggan'}
+                        ]
+                    }
+                ]
+            },
+        ]
     },
 
     created() {
     },
 
-    methods: {
-
-    }
-});
+    methods: {}
+})
 
--- src/cascader/cascader.vue
+++ src/cascader/cascader.vue
@@ -1,14 +1,32 @@
 <template>
-    
+    <div class="cascader-content">
+        <div class="trigger">
+            <slot></slot>
+        </div>
+        <div class="popover">
+<!--        递归组件-->
+            <cascader-item v-for="(item, index) in source" :key="index" :sourceItem="item"></cascader-item>
+        </div>
+    </div>
 </template>
 
 <script>
+    import cascaderItem from './cascader-item'
+
     export default {
-        name: 'cascader'
+        name: 'cascader',
+        components: {cascaderItem},
+        props: {
+            source: {
+                type: Array
+            }
+        },
     }
 </script>
 
 <style lang="scss" scoped>
+    .cascader-content {
 
+    }
 
 </style>
