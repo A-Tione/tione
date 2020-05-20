4daf50094b171561d6ad75ff85c8ed35d8ce68c6
a-tione <heyanggo@gmail.com>
2020-05-20 21:03:40 +0800

    将表单校验if逻辑抽成函数

--- src/validate.js
+++ src/validate.js
@@ -3,28 +3,47 @@ export default function validate (data, rules) {
     rules.forEach(rule => {
         let value = data[rule.key]
         if (rule.required) {
-            if (value !== 0 && !value) {
-                errors[rule.key] = {required: '必填'}
-                return
+            let error = validate.required(value)
+            if (error) {
+                ensureObject(errors, rule.key)
+                errors[rule.key].required = error
             }
         }
         if (rule.pattern) {
-            if (rule.pattern === 'email') {
-                rule.pattern = /^.+@.+$/
-            }
-            if (rule.pattern.test(value) === false) {
+            let error = validate.pattern(value, rule.pattern)
+            if (error) {
                 ensureObject(errors, rule.key)
-                errors[rule.key] = {pattern: '格式不正确'}
+                errors[rule.key].pattern = error
             }
         }
         if (rule.minLength) {
-            if (value.length < rule.minLength) {
+            let error = validate.minLength(value, rule.pattern)
+            if (error) {
                 ensureObject(errors, rule.key)
-                errors[rule.key].minLength = '太短'
+                errors[rule.key].minLength = error
             }
         }
     })
     return errors
+}
+
+validate.required = (value) => {
+    if (value !== 0 && !value) {
+        return '必填'
+    }
+}
+validate.pattern = (value, pattern) => {
+    if (value !== 'email') {
+        pattern = /^.+@.+$/
+    }
+    if (pattern.test(value) === false) {
+        return '格式不正确'
+    }
+}
+validate.minLength = (value, pattern) => {
+    if (value.length < pattern) {
+        return '太短'
+    }
 }
 
 function ensureObject (obj, key) {
