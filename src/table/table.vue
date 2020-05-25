b6d24ff642d8b88244520a2ecab61317b00ed20e
a-tione <heyanggo@gmail.com>
2020-05-25 22:56:23 +0800

    table组件框架搭建

---
+++ src/table/table.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="table-content">
+        <table></table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'table',
+        props: {
+            colums: {
+                type: Array,
+                required: true
+            },
+            dataSource: {
+                type: Array,
+                required: true
+            }
+        },
+        data() {
+            return {}
+        },
+        methods: {}
+    }
+</script>
+
+<style lang="scss" scoped>
+    .table-content {
+
+    }
+
+</style>
