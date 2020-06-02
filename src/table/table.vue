2ee8c2e9302bbf7b3bf6b9cdcab73dab51700084
a-tione <heyanggo@gmail.com>
2020-06-02 22:11:35 +0800

    table.vue 组件增加checked 全选半选的功能

--- src/table/table.vue
+++ src/table/table.vue
@@ -1,6 +1,28 @@
 <template>
     <div class="pages-content">
-        <table></table>
+        <table class="pages-content-table" :class="{bordered, compact, striped: striped}">
+            <thead>
+            <tr>
+                <th><input ref="allChecked" type="checkbox" @change="onChangeAllItems"/></th>
+                <th v-if="numberVisible">#</th>
+                <th v-for="column in column">
+                    {{column.text}}
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(item,index) in dataSource">
+                <td>
+                    <input type="checkbox" @change="onChangeItem(item, index, $event)"
+                    :checked="inSelectedItems(item)">
+                </td>
+                <td v-if="numberVisible">{{index+1}}</td>
+                <template v-for="column in columns">
+                    <td>{{item[column.field]}}</td>
+                </template>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -8,25 +30,90 @@
     export default {
         name: 'table',
         props: {
-            colums: {
+            striped: {
+                type: Boolean,
+                default: true
+            },
+            selectedItems: {
+                type: Array,
+                default: ()=> []
+            },
+            compact: {
+                type: Boolean,
+                default: false
+            },
+            columns: {
                 type: Array,
                 required: true
             },
             dataSource: {
                 type: Array,
-                required: true
+                required: true,
+                validator(array) {
+                    return !(array.filter(item => item.id === undefined).length > 0)
+                }
+            },
+            numberVisible: {
+                type: Boolean,
+                default: false
+            },
+            bordered: {
+                type: Boolean,
+                default: false
             }
         },
+
         data() {
             return {}
         },
-        methods: {}
+
+        watch: {
+            selectedItems() {
+                if (this.selectedItems.length === this.dataSource.length) {
+                    this.$refs.allChecked.indeterminate = false
+                } else this.$refs.allChecked.indeterminate = this.selectedItems.length !== 0;
+            }
+        },
+
+        methods: {
+            inSelectedItems(item) {
+                return this.selectedItems.filter(value => value.id === item.id).length > 0
+            },
+            onChangeItem(item, index, e) {
+                let selected = e.target.checked
+                let copy = JSON.parse(JSON.stringify(this.selectedItems))
+                if (selected) {
+                    copy.push(item)
+                } else {
+                    copy.splice(copy.indexOf(item), 1)
+                }
+                this.$emit('update:selectedItems', copy)
+            },
+            onChangeAllItems (e) {
+                let selected = e.target.checked
+                 this.$emit('update:selectedItems', selected? this.dataSource : [])
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-    .pages-content {
+    @import 'styles/var';
+    $grey: darken($grey, 10%);
 
+    .pages-content {
+        &-table {
+            width: 100%;
+            border-collapse: collapse;
+            border-spacing: 0;
+            border-bottom: 1px solid $grey;
+            &.bordered {
+                border: 1px solid $grey;
+                td, th {
+                    border: 1px solid $grey;
+                }
+            }
+        }
     }
 
 </style>
